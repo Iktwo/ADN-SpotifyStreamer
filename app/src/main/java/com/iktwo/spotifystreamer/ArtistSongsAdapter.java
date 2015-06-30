837ad687af2349413873dbde8d36cf2baef673a0
Iktwo SH <iktwo.sh@gmail.com>
2015-06-30 08:40:22 -0700

    Update ArtistSong delegate and Adapter

--- app/src/main/java/com/iktwo/spotifystreamer/ArtistSongsAdapter.java
+++ app/src/main/java/com/iktwo/spotifystreamer/ArtistSongsAdapter.java
@@ -8,6 +8,8 @@ import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.squareup.picasso.Picasso;
+
 import java.util.List;
 
 import kaaes.spotify.webapi.android.models.Track;
@@ -28,7 +30,8 @@ public class ArtistSongsAdapter extends ArrayAdapter<Track> {
             convertView = LayoutInflater.from(getContext()).inflate(R.layout.song_delegate, parent, false);
 
             holder = new ViewHolder();
-            holder.artist = (TextView) convertView.findViewById(R.id.text_view_name);
+            holder.song = (TextView) convertView.findViewById(R.id.text_view_name);
+            holder.album = (TextView) convertView.findViewById(R.id.text_view_album);
             holder.thumbnail = (ImageView) convertView.findViewById(R.id.image_view_thumbnail);
 
             convertView.setTag(holder);
@@ -36,81 +39,30 @@ public class ArtistSongsAdapter extends ArrayAdapter<Track> {
             holder = (ViewHolder) convertView.getTag();
         }
 
-        holder.artist.setText(track.name);
+        holder.song.setText(track.name);
+        holder.album.setText(track.album.name);
 
-        /* if (!artistResult.artist.images.isEmpty()) {
-            String url = artistResult.artist.images.get(0).url.replace("55x55", "400x400");
-            holder.thumbnail.setTag(artistResult.artist.images.get(0).url);
+        if (!track.album.images.isEmpty()) {
+            String url = track.album.images.get(0).url;
 
-            if (artistResult.getBackgroundColor() == 0) {
-                Picasso.with(mContext)
-                        .load(url)
-                        .into(holder.thumbnail,
-                                PicassoPalette.with(url, holder.thumbnail)
-                                        .use(PicassoPalette.Profile.VIBRANT)
-                                        .intoBackground(holder.background, PicassoPalette.Swatch.RGB)
-                                        .intoTextColor(holder.artist, PicassoPalette.Swatch.TITLE_TEXT_COLOR)
-                                        .intoCallBack(
-                                                new PicassoPalette.CallBack() {
-                                                    @Override
-                                                    public void onPaletteLoaded(Palette palette) {
-                                                        Palette.Swatch s = palette.getVibrantSwatch();
+            holder.thumbnail.setTag(url);
 
-                                                        // If there is a vibrant color, do nothing
-                                                        if (s != null) {
-                                                            artistResult.setBackgroundColor(s.getRgb());
-                                                            artistResult.setTextColor(s.getBodyTextColor());
-                                                            artistResult.setTitleColor(s.getTitleTextColor());
-                                                            return;
-                                                        }
-
-                                                        for (int i = 0; i < 5; i++) {
-                                                            switch (i) {
-                                                                case 0:
-                                                                    s = palette.getDarkVibrantSwatch();
-                                                                    break;
-                                                                case 1:
-                                                                    s = palette.getLightVibrantSwatch();
-                                                                    break;
-                                                                case 2:
-                                                                    s = palette.getMutedSwatch();
-                                                                    break;
-                                                                case 3:
-                                                                    s = palette.getDarkMutedSwatch();
-                                                                    break;
-                                                                case 4:
-                                                                    s = palette.getLightMutedSwatch();
-                                                                    break;
-                                                            }
-
-                                                            if (s != null) {
-                                                                holder.background.setBackgroundColor(s.getRgb());
-                                                                holder.artist.setTextColor(s.getTitleTextColor());
-
-                                                                artistResult.setBackgroundColor(s.getRgb());
-                                                                artistResult.setTextColor(s.getBodyTextColor());
-                                                                artistResult.setTitleColor(s.getTitleTextColor());
-
-                                                                return;
-                                                            }
-                                                        }
-                                                    }
-                                                }));
-            } else {
-                Picasso.with(mContext)
-                        .load(url)
-                        .into(holder.thumbnail);
-
-                holder.background.setBackgroundColor(artistResult.getBackgroundColor());
-                holder.artist.setTextColor(artistResult.getTextColor());
-            }
-        } */
+            Picasso.with(getContext())
+                    .load(url)
+                    .placeholder(R.drawable.placeholder_artist)
+                    .into(holder.thumbnail);
+        } else {
+            Picasso.with(getContext())
+                    .load(R.drawable.placeholder_artist)
+                    .into(holder.thumbnail);
+        }
 
         return convertView;
     }
 
     static class ViewHolder {
-        public TextView artist;
+        public TextView song;
+        public TextView album;
         public ImageView thumbnail;
     }
 }
